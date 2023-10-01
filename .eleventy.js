2460b1537ac2fab16f982434de5059179d072434
uille <ville@vartiainenofficial.eu>
2023-10-01 23:21:11 +0300

    Removed url filter from index page's links, added htmlbase plugin to .eleventy.js to modify urls with pathprefix automagically

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,14 @@
+// Add the htmlbase plugin to add path prefix to all urls automagically
+// This is needed with Github Pages, since GH pages has the repo name in the path, e.g. /reponame/index.html
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.setBrowserSyncConfig();
     
     // Rn the style is just included in the template
     //eleventyConfig.addPassthroughCopy("_includes/style.css");
+
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
     // Add the interlinker plugin to process obsidian-style links and backlinks
     eleventyConfig.addPlugin(
